37d947b400e0002b73b72f2ac160493171e70fde
Nick-0201 <muses1boy@gmail.com>
2023-06-02 14:17:31 +0600

    my first co

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// const nickname = "Almaz"
+// var age = "18"
+// var dataMonthe = "april"
+// console.log(nickname, age, dataMonthe)
+
+// const question = [
+//   prompt("Ты ?"),
+//   prompt("Кто "),
+//   prompt("как ты живешь"),
+// ]
+// console.log(question);
+
+// var question = {
+//   name: prompt("как вас зовут ?"),
+//   age: prompt("сколько вам лет ?"),
+//   sex: prompt("какого вы пола?")
+// }
+// console.log(question);
+
+// ==================================================================================================
+// const arr = ["nick1", "nick2", "nick3", "nick4", "nick5", "nick6", "nick7", "nick8", "nick",];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i] == "nick");
+// }
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr == "nick");
+// }
+// ==================================================================================================
+const test = document.querySelectorAll("li");
+console.log(test);

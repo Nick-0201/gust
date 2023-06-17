d786549acc4ba0185389f22411fefc3bd0f004a5
Nick-0201 <nurmukhammad93@icloud.com>
2023-06-18 03:14:25 +0600

    humburgerMenu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-console.log("dsfsdf");
+const burger = document.querySelector(".header__burger");
+const navmenu = document.querySelector(".header__list");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  navmenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".header__link").forEach(i =>
+  i.addEventListener("click", () => {
+    burger.classList.remove("active");
+    navmenu.classList.remove("active");
+  }))

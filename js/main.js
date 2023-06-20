acb10c1766a07b58ed4c8152b2eaba34dfe3ddff
Nick-0201 <nurmukhammad93@icloud.com>
2023-06-20 14:22:53 +0600

    img text gallary

--- js/main.js
+++ js/main.js
@@ -1,13 +1,2 @@
-const burger = document.querySelector(".header__burger");
-const navmenu = document.querySelector(".header__list");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  navmenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".header__link").forEach(i =>
-  i.addEventListener("click", () => {
-    burger.classList.remove("active");
-    navmenu.classList.remove("active");
-  }))
+import { humburgerExpo } from './humburger.js';
+humburgerExpo();
